f5089ab24469834dbee538abc2b43aea0f56983f
pooja sahu <poojasahu09312@gmail.com>
2025-11-21 21:51:28 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class array{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+            System.out.print("enter the size:");
+            int n = sc.nextInt();
+            System.out.print("enter the array element:");
+            int[]arr = new int[n];
+            for(int i=0; i<n; i++){
+                 arr[i] = sc.nextInt();
+            }
+            for (int i = 0; i<n; i++){
+                System.out.print(arr[i] + " ");
+
+            }
+                
+        }
+                
+   }
+        
+
+        
+    
